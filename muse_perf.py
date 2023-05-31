ffb67fbde8698623c8e034670d764994e356995a
William Berman <WLBberman@gmail.com>
2023-05-31 13:06:47 -0700

    benchmark full pipeline

--- muse_perf.py
+++ muse_perf.py
@@ -1,5 +1,6 @@
 from muse.modeling_taming_vqgan import VQGANModel
 from muse.modeling_transformer import MaskGiTUViT
+from muse import PipelineMuse
 
 from transformers import CLIPTextModel, AutoTokenizer
 
@@ -12,12 +13,11 @@ torch.set_grad_enabled(False)
 torch.set_float32_matmul_precision("high")
 
 num_threads = torch.get_num_threads()
-model = "openMUSE/muse-cc12m-uvit-clip-130k"
+model = "openMUSE/muse-laiona6-uvit-clip-220k"
+prompt = "A high tech solarpunk utopia in the Amazon rainforest"
 
 
 def make_encoder_hidden_states(*, device, dtype, batch_size, tokenizer, text_encoder):
-    prompt = "A high tech solarpunk utopia in the Amazon rainforest"
-
     text_tokens = tokenizer(
         prompt,
         return_tensors="pt",
@@ -105,6 +105,30 @@ def benchmark_vae(*, device, batch_size, dtype, compiled, vae):
 
     def benchmark_fn():
         vae.decode_code(image_tokens)
+
+    if compiled:
+        benchmark_fn()
+
+    out = Timer(
+        stmt="benchmark_fn()",
+        globals={"benchmark_fn": benchmark_fn},
+        num_threads=num_threads,
+        label=label,
+        description=description,
+    ).blocked_autorange(min_run_time=1)
+
+    return out
+
+def benchmark_full(*, batch_size, dtype, compiled, pipe):
+    label = f"{model}, full pipeline, batch_size: {batch_size}, dtype: {dtype}"
+    description = f"compiled {compiled}"
+
+    print("*******")
+    print(label)
+    print(description)
+
+    def benchmark_fn():
+        pipe(prompt, num_images_per_prompt=batch_size, timesteps=12)
 
     if compiled:
         benchmark_fn()
@@ -212,14 +236,71 @@ def main_vae(device, file):
         f.write(out)
 
 
+full_params = {
+    "cuda": {
+        "batch_size": [1, 2, 4, 8, 16, 32],
+        "dtype": [torch.float16],
+        "compiled": [False, True],
+    },
+    "cpu": {
+        "batch_size": [1, 2, 4, 8],
+        "dtype": [torch.float32],
+        "compiled": [False, True],
+    },
+}
+
+def main_full(device, file):
+    results = []
+
+    tokenizer = AutoTokenizer.from_pretrained(model, subfolder="text_encoder")
+
+    for dtype in vae_params[device]["dtype"]:
+        text_encoder = CLIPTextModel.from_pretrained(model, subfolder="text_encoder").to(
+            device=device, dtype=dtype
+        )
+
+        for batch_size in vae_params[device]["batch_size"]:
+            for compiled in vae_params[device]["compiled"]:
+                vae = make_vae(device, compiled, dtype)
+                transformer = make_transformer(device, compiled, dtype)
+
+                pipe = PipelineMuse(
+                    tokenizer=tokenizer,
+                    text_encoder=text_encoder,
+                    vae=vae,
+                    transformer=transformer
+                )
+
+                bm = benchmark_full(
+                    device=device,
+                    dtype=dtype,
+                    compiled=compiled,
+                    batch_size=batch_size,
+                    pipe=pipe,
+                )
+
+                results.append(bm)
+
+    out = str(Compare(results))
+
+    with open(file, "w") as f:
+        f.write(out)
+
+
 parser = ArgumentParser()
-parser.add_argument("--model", required=True)
+parser.add_argument("--model", required=False)
+parser.add_argument("--full", required=False, type="set_true")
 parser.add_argument("--device", required=True)
 parser.add_argument("--file", required=True)
 
 args = parser.parse_args()
 
-if args.model == "transformer":
-    main_transformer(args.device, args.file)
-elif args.model == "vae":
-    main_vae(args.device, args.file)
+if args.full:
+    main_full(args.device, args.file)
+else:
+    if args.model == "transformer":
+        main_transformer(args.device, args.file)
+    elif args.model == "vae":
+        main_vae(args.device, args.file)
+    else:
+        assert False
--- run.sh
+++ run.sh
@@ -6,5 +6,8 @@ GPU="${1}"
 
 python muse_perf.py --model transformer --device cuda --file transformer_${GPU}.txt
 python muse_perf.py --model vae --device cuda --file vae_${GPU}.txt
+python muse_perf.py --full --device cuda --file full_${GPU}.txt
+
 python muse_perf.py --model transformer --device cpu --file transformer_cpu.txt
 python muse_perf.py --model vae --device cpu --file vae_cpu.txt
+python muse_perf.py --full --device cpu --file full_cpu.txt
