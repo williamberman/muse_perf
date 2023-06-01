5b80664c8595ae4240e29c41e07f08596bf30098
Will Berman <WLBberman@gmail.com>
2023-06-01 00:51:56 +0000

    run script update to backbone

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ set -e
 
 GPU="${1}"
 
-python muse_perf.py --model transformer --device cuda --file transformer_${GPU}.txt
+python muse_perf.py --model backbone --device cuda --file backbone_${GPU}.txt
 python muse_perf.py --model vae --device cuda --file vae_${GPU}.txt
 python muse_perf.py --full --device cuda --file full_${GPU}.txt
 
-python muse_perf.py --model transformer --device cpu --file transformer_cpu.txt
+python muse_perf.py --model backbone --device cpu --file backbone_cpu.txt
 python muse_perf.py --model vae --device cpu --file vae_cpu.txt
 python muse_perf.py --full --device cpu --file full_cpu.txt
