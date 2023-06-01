7669825921e8146c7a030740a7dcfde639234f3f
Will Berman <WLBberman@gmail.com>
2023-06-01 06:08:03 +0000

    t4 runner scripts

---
+++ run_t4_vae.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set -e
+
+python muse_perf.py --dtype float16 --batch_size 1 --compiled False --model vae --device cuda --file vae_t4.txt
+python muse_perf.py --dtype float16 --batch_size 1 --compiled True --model vae --device cuda --file vae_t4.txt
+
+python muse_perf.py --dtype float16 --batch_size 2 --compiled False --model vae --device cuda --file vae_t4.txt
+python muse_perf.py --dtype float16 --batch_size 2 --compiled True --model vae --device cuda --file vae_t4.txt
+
+python muse_perf.py --dtype float16 --batch_size 4 --compiled False --model vae --device cuda --file vae_t4.txt
+python muse_perf.py --dtype float16 --batch_size 4 --compiled True --model vae --device cuda --file vae_t4.txt
+
+python muse_perf.py --dtype float16 --batch_size 8 --compiled False --model vae --device cuda --file vae_t4.txt
+python muse_perf.py --dtype float16 --batch_size 8 --compiled True --model vae --device cuda --file vae_t4.txt
+
+python muse_perf.py --dtype float16 --batch_size 16 --compiled False --model vae --device cuda --file vae_t4.txt
+python muse_perf.py --dtype float16 --batch_size 16 --compiled True --model vae --device cuda --file vae_t4.txt
+
+python muse_perf.py --dtype float16 --batch_size 32 --compiled False --model vae --device cuda --file vae_t4.txt
+python muse_perf.py --dtype float16 --batch_size 32 --compiled True --model vae --device cuda --file vae_t4.txt
