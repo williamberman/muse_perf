08c80cff282369f7d2b7df6d86d97f964eafdfd3
Will Berman <WLBberman@gmail.com>
2023-06-01 20:36:45 +0000

    better run full script

---
+++ run_full.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -e
+set -u
+
+GPU="${1}"
+
+python muse_perf.py --dtype float16 --batch_size 1 --full --device cuda --file full_$GPU.txt
+
+python muse_perf.py --dtype float16 --batch_size 2 --full --device cuda --file full_$GPU.txt
+
+python muse_perf.py --dtype float16 --batch_size 4 --full --device cuda --file full_$GPU.txt
+
+python muse_perf.py --dtype float16 --batch_size 8 --full --device cuda --file full_$GPU.txt
+
+python muse_perf.py --dtype float16 --batch_size 16 --full --device cuda --file full_$GPU.txt
+
+python muse_perf.py --dtype float16 --batch_size 32 --full --device cuda --file full_$GPU.txt
