654c52634984333e34b1fd95ced7d686d6d12181
Will Berman <WLBberman@gmail.com>
2023-06-02 01:53:13 +0000

    full t4

---
+++ run_full_t4.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+set -e
+set -u
+
+GPU=t4
+
+python muse_perf.py --batch_size 1 --full --device cuda --file full_$GPU.txt
+python muse_perf.py --batch_size 2 --full --device cuda --file full_$GPU.txt
+
+python muse_perf.py --batch_size 4 --full --device cuda --file full_$GPU.txt --compiled None
+python muse_perf.py --batch_size 4 --full --device cuda --file full_$GPU.txt --compiled default
+python muse_perf.py --batch_size 4 --full --device cuda --file full_$GPU.txt --compiled reduce-overhead --model muse_f16
+
+python muse_perf.py --batch_size 8 --full --device cuda --file full_$GPU.txt --compiled None
+python muse_perf.py --batch_size 8 --full --device cuda --file full_$GPU.txt --compiled default
+python muse_perf.py --batch_size 8 --full --device cuda --file full_$GPU.txt --compiled reduce-overhead --model muse_f16
+
+python muse_perf.py --batch_size 16 --full --device cuda --file full_$GPU.txt --compiled None
+python muse_perf.py --batch_size 16 --full --device cuda --file full_$GPU.txt --compiled default
+python muse_perf.py --batch_size 16 --full --device cuda --file full_$GPU.txt --compiled reduce-overhead --model muse_f16
+
+python muse_perf.py --batch_size 32 --full --device cuda --file full_$GPU.txt --model muse_f16
