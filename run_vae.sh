dce1ee20a3d76a49c65468370e71338a265d0159
Will Berman <WLBberman@gmail.com>
2023-06-02 02:02:41 +0000

    vae cpu

--- run_vae.sh
+++ run_vae.sh
@@ -4,15 +4,17 @@ set -e
 set -u
 
 GPU="${1}"
+DEVICE="${2}"
 
-python muse_perf.py --dtype float16 --batch_size 1 --component vae --device cuda --file vae_$GPU.txt
+python muse_perf.py --batch_size 1 --component vae --device $DEVICE --file vae_$GPU.txt
 
-python muse_perf.py --dtype float16 --batch_size 2 --component vae --device cuda --file vae_$GPU.txt
+python muse_perf.py --batch_size 2 --component vae --device $DEVICE --file vae_$GPU.txt
 
-python muse_perf.py --dtype float16 --batch_size 4 --component vae --device cuda --file vae_$GPU.txt
+python muse_perf.py --batch_size 4 --component vae --device $DEVICE --file vae_$GPU.txt
 
-python muse_perf.py --dtype float16 --batch_size 8 --component vae --device cuda --file vae_$GPU.txt
+python muse_perf.py --batch_size 8 --component vae --device $DEVICE --file vae_$GPU.txt
 
-python muse_perf.py --dtype float16 --batch_size 16 --component vae --device cuda --file vae_$GPU.txt
+python muse_perf.py --batch_size 16 --component vae --device $DEVICE --file vae_$GPU.txt
 
-python muse_perf.py --dtype float16 --batch_size 32 --component vae --device cuda --file vae_$GPU.txt
+python muse_perf.py --batch_size 32 --component vae --device $DEVICE --file vae_$GPU.txt
+
