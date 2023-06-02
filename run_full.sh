0432d0bed7021454fb7f71927bf8701b9ef28229
Will Berman <WLBberman@gmail.com>
2023-06-02 02:22:17 +0000

    run full update

--- run_full.sh
+++ run_full.sh
@@ -5,14 +5,14 @@ set -u
 
 GPU="${1}"
 
-python muse_perf.py --dtype float16 --batch_size 1 --full --device cuda --file full_$GPU.txt
+python muse_perf.py --batch_size 1 --full --device cuda --file full_$GPU.txt
 
-python muse_perf.py --dtype float16 --batch_size 2 --full --device cuda --file full_$GPU.txt
+python muse_perf.py --batch_size 2 --full --device cuda --file full_$GPU.txt
 
-python muse_perf.py --dtype float16 --batch_size 4 --full --device cuda --file full_$GPU.txt
+python muse_perf.py --batch_size 4 --full --device cuda --file full_$GPU.txt
 
-python muse_perf.py --dtype float16 --batch_size 8 --full --device cuda --file full_$GPU.txt
+python muse_perf.py --batch_size 8 --full --device cuda --file full_$GPU.txt
 
-python muse_perf.py --dtype float16 --batch_size 16 --full --device cuda --file full_$GPU.txt
+python muse_perf.py --batch_size 16 --full --device cuda --file full_$GPU.txt
 
-python muse_perf.py --dtype float16 --batch_size 32 --full --device cuda --file full_$GPU.txt
+python muse_perf.py --batch_size 32 --full --device cuda --file full_$GPU.txt
