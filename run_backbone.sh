2a99cdc0c5052f25380ec6fdffc8e2c4a81650c6
Will Berman <WLBberman@gmail.com>
2023-06-02 00:02:55 +0000

    updae cli args

--- run_backbone.sh
+++ run_backbone.sh
@@ -5,14 +5,14 @@ set -u
 
 GPU="${1}"
 
-python muse_perf.py --dtype float16 --batch_size 1 --model backbone --device cuda --file backbone_$GPU.txt
+python muse_perf.py --dtype float16 --batch_size 1 --component backbone --device cuda --file backbone_$GPU.txt
 
-python muse_perf.py --dtype float16 --batch_size 2 --model backbone --device cuda --file backbone_$GPU.txt
+python muse_perf.py --dtype float16 --batch_size 2 --component backbone --device cuda --file backbone_$GPU.txt
 
-python muse_perf.py --dtype float16 --batch_size 4 --model backbone --device cuda --file backbone_$GPU.txt
+python muse_perf.py --dtype float16 --batch_size 4 --component backbone --device cuda --file backbone_$GPU.txt
 
-python muse_perf.py --dtype float16 --batch_size 8 --model backbone --device cuda --file backbone_$GPU.txt
+python muse_perf.py --dtype float16 --batch_size 8 --component backbone --device cuda --file backbone_$GPU.txt
 
-python muse_perf.py --dtype float16 --batch_size 16 --model backbone --device cuda --file backbone_$GPU.txt
+python muse_perf.py --dtype float16 --batch_size 16 --component backbone --device cuda --file backbone_$GPU.txt
 
-python muse_perf.py --dtype float16 --batch_size 32 --model backbone --device cuda --file backbone_$GPU.txt
+python muse_perf.py --dtype float16 --batch_size 32 --component backbone --device cuda --file backbone_$GPU.txt
